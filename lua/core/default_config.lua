4d50833665dfd335e6e25a3cc76fc7f16f2a7653
re1san <kathareaniket47@gmail.com>
2023-12-30 13:30:38 +0530

    Ik

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -10,8 +10,8 @@ M.ui = {
   hl_add = {},
   hl_override = {},
   changed_themes = {},
-  theme_toggle = { "tsuki", "tsuki" },
-  theme = "tsuki", -- default theme
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark", -- default theme
   transparency = false,
   lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
